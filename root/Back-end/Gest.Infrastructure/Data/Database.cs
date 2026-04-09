55b0449841abe50ae7c9a071ed49c49ada16943b
AdalbertoAo <4dalbertosil@gmail.com>
2026-04-09 14:15:08 +0100

    refactor(backend): move database.cs to `Gest.infra/Data`

---
+++ root/Back-end/Gest.Infrastructure/Data/Database.cs
@@ -0,0 +1,17 @@
+using Npgsql;
+
+public class Database
+{
+    private readonly string _connectionString;
+
+    public Database(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public NpgsqlConnection GetConnection()
+    {
+        return new NpgsqlConnection(_connectionString);
+    }
+}
+
